f947b012a388d749e59e25a09476146ef653280a
Gilbert Keen <gilbert.keen@gmail.com>
2018-05-22 06:30:34 +0300

    add business logic

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,4 +1,19 @@
 //Business Logic
+var numbers = [];
+function PingPong(number){
+    for (var i = 1; i <= number; i++){
+        if ((i % 3 === 0)&&(i % 5 ===0)){
+            numbers.push("pingpong")
+        } else if (i % 3 === 0) {
+            numbers.push("ping")
+        } else if (i % 5 === 0) {
+            numbers.push("pong")
+        } else {
+            numbers.push(i)
+        }
+    }
+    return numbers;
+}
 
 //User-Interface Logic
 $(document).ready(function(){
